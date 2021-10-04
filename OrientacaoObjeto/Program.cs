7ac9fa26e2ebfc37c2ea92983f261473750d87e3
Eduardo Alves <edu.alves88@outlook.com>
2021-10-04 15:00:00 -0300

    código foi comentado

--- OrientacaoObjeto/Program.cs
+++ OrientacaoObjeto/Program.cs
@@ -1,41 +1,45 @@
 ﻿using System;
 using System.Globalization;
+
 namespace OrientacaoObjeto
+{
+    class Program
     {
-        class Program
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
+            //Instância da Classe Tringulo
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+            //Entrada de Dados
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Entrada de Dados
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Realização do cálculo
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+            //Print dos valores de área já calculados pela Classe
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+            //Definição de qual área é a maior
+            if (areaX > areaY)
             {
-                Triangulo x, y;
-                x = new Triangulo();
-                y = new Triangulo();
-                Console.WriteLine("Entre com as medidas do triângulo X:");
-                x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                Console.WriteLine("Entre com as medidas do triângulo Y:");
-                y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                double p = (x.A + x.B + x.C) / 2.0;
-                double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
-                p = (y.A + y.B + y.C) / 2.0;
-                double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
-
-                Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
-                Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
-                if (areaX > areaY)
-                {
-                    Console.WriteLine("Maior área: X");
-                }
-                else
-                {
-                    Console.WriteLine("Maior área: Y");
-                }
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
             }
         }
     }
+}
 
 
